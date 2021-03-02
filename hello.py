db0c26aa14e57a11769023557bad35a517f5e657
kblaney <kennethrblaney@gmail.com>
2021-03-01 19:43:11 -0500

    add html

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello World."
+    return render_template("sample.html")
 
 
 @app.route('/hello/<name>', methods=['GET', 'POST'])
